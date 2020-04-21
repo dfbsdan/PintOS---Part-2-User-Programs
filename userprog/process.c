66c0bce8d62391d894fb3f8d51f3c0f2fe579cd6
Daniel <dfblancos@kaist.ac.kr>
2020-04-22 06:16:23 +0900

    argPass 0.5.4

--- userprog/process.c
+++ userprog/process.c
@@ -626,13 +626,12 @@ setup_stack (struct intr_frame *if_, const int argc, char **argv) {
 				esp -= sizeof (char*);
 				memcpy (esp, &argv[i], sizeof (char*));
 		}
-		/* Set RSI. */
+		/* Set registers. */
+		if_->R.rdi = (uint64_t)argc;
 		if_->R.rsi = (uint64_t)esp;
 		/* Leave a space for the return address. */
 		esp -= sizeof (void*);
 		memset (esp, 0, sizeof (void*));
-		/* Set RDI. */
-		if_->R.rdi = (uint64_t)argc;
 
 		for (uint8_t *i = (uint8_t*)(USER_STACK -1); i >= esp; i--)
 			printf("i: 0x%x, val: 0x%x, char: %c\n", i, *i, *i);
