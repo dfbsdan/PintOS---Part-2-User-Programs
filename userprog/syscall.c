2cd8155aa583ae186eb9bddd77c75ef46afb654f
Daniel <dfblancos@kaist.ac.kr>
2020-04-22 15:15:50 +0900

    syscall 0.3: create and remove syscalls added. Address-checking function as well

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -7,6 +7,7 @@
 #include "userprog/gdt.h"
 #include "threads/flags.h"
 #include "threads/init.h"
+#include "threads/vaddr.h"
 #include "intrinsic.h"
 
 void syscall_entry (void);
@@ -40,6 +41,11 @@ static void syscall_seek (int fd, unsigned position);
 static unsigned syscall_tell (int fd);
 static void syscall_close (int fd);
 static int syscall_dup2 (int oldfd, int newfd);
+////////////////////////////////////////////////////////////////////////////////////////////////////////TESTING
+static void check_address(void *addr);
+static int get_user(const uint8_t *uaddr);
+static bool put_user(uint8_t *udst, uint8_t byte);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void
 syscall_init (void) {
@@ -218,8 +224,8 @@ syscall_wait (int pid UNUSED) {
 * not open it: opening the new file is a separate operation which would
 * require a open system call. */
 static bool
-syscall_create (const char *file UNUSED, unsigned initial_size UNUSED) {
-	ASSERT (0);
+syscall_create (const char *file, unsigned initial_size) {
+	return filesys_create(file, initial_size);
 }
 
 /* Deletes the file called file. Returns true if successful, false
@@ -227,8 +233,8 @@ syscall_create (const char *file UNUSED, unsigned initial_size UNUSED) {
  * closed, and removing an open file does not close it. See Removing an
  * Open File in FAQ for details. */
 static bool
-syscall_remove (const char *file UNUSED) {
-	ASSERT (0);
+syscall_remove (const char *file) {
+	return filesys_remove(file);
 }
 
 /* Opens the file called file. Returns a nonnegative integer handle called
@@ -329,4 +335,33 @@ syscall_close (int fd UNUSED) {
 static int
 syscall_dup2 (int oldfd UNUSED, int newfd UNUSED) {
 	ASSERT (0);
+}
+
+static void
+check_address (void *addr) {
+	if (!is_user_vaddr(addr)) /* addr is not in user va. */
+		syscall_exit (-1);
+}
+
+/* Reads a byte at user virtual address UADDR.
+ * UADDR must be below KERN_BASE.
+ * Returns the byte value if successful, -1 if a segfault
+ * occurred. */
+static int
+get_user (const uint8_t *uaddr) {
+    int result;
+    asm ("movl $1f, %0; movzbl %1, %0; 1:"
+         : "=&a" (result) : "m" (*uaddr));
+    return result;
+}
+
+/* Writes BYTE to user address UDST.
+ * UDST must be below KERN_BASE.
+ * Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte) {
+    int error_code;
+    asm ("movl $1f, %0; movb %b2, %1; 1:"
+    : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+    return error_code != -1;
 }
