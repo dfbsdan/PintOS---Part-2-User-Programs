dfe3e94f21c514e2bf50dea38974d3e7fa8586ac
Daniel <dfblancos@kaist.ac.kr>
2020-04-22 15:33:07 +0900

    syscall 0.3.2

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -338,31 +338,34 @@ syscall_dup2 (int oldfd UNUSED, int newfd UNUSED) {
 	ASSERT (0);
 }
 
-static void
-check_address (void *addr) {
-	if (!is_user_vaddr(addr)) /* addr is not in user va. */
-		syscall_exit (-1);
-}
+//UNCOMMENT WHEN USED
+
+//static void
+//check_address (void *addr) {
+//	if (!is_user_vaddr(addr)) /* addr is not in user va. */
+//		syscall_exit (-1);
+//}
+
 
 /* Reads a byte at user virtual address UADDR.
  * UADDR must be below KERN_BASE.
  * Returns the byte value if successful, -1 if a segfault
  * occurred. */
-static int
-get_user (const uint8_t *uaddr) {
-    int result;
-    asm ("movl $1f, %0; movzbl %1, %0; 1:"
-         : "=&a" (result) : "m" (*uaddr));
-    return result;
-}
+//static int
+//get_user (const uint8_t *uaddr) {
+    //int result;
+    //asm ("movl $1f, %0; movzbl %1, %0; 1:"
+         //: "=&a" (result) : "m" (*uaddr));
+    //return result;
+//}
 
 /* Writes BYTE to user address UDST.
  * UDST must be below KERN_BASE.
  * Returns true if successful, false if a segfault occurred. */
-static bool
-put_user (uint8_t *udst, uint8_t byte) {
-    int error_code;
-    asm ("movl $1f, %0; movb %b2, %1; 1:"
-    : "=&a" (error_code), "=m" (*udst) : "q" (byte));
-    return error_code != -1;
-}
+//static bool
+//put_user (uint8_t *udst, uint8_t byte) {
+    //int error_code;
+    //asm ("movl $1f, %0; movb %b2, %1; 1:"
+    //: "=&a" (error_code), "=m" (*udst) : "q" (byte));
+    //return error_code != -1;
+//}
