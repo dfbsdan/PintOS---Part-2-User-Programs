34901f77f6b5bd6bb30d3f46bca79479a4f7098b
Daniel <dfblancos@kaist.ac.kr>
2020-04-24 18:26:39 +0900

    syscall 0.7.11

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -67,6 +67,7 @@ void
 syscall_handler (struct intr_frame *f) {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////TESTING
 	ASSERT (thread_is_user ());
+	printf("SYSCALL!!\n");
 	switch (f->R.rax) {
 		case SYS_HALT:
 			syscall_halt ();
